f84986f53f1399a7b588e2fce406a3adb80714eb
Viktor Dimitrov <viktor.dimitrov.1989@gmail.com>
2016-12-07 12:09:26 +0200

    meeting/Meeting.css finishing

--- src/components/meeting/MeetingPage.js
+++ src/components/meeting/MeetingPage.js
@@ -13,22 +13,23 @@ export default class MemberProjectsPage extends Component {
         this.onLoadSuccess = this.onLoadSuccess.bind(this);
     }
 
-    componentDidMount(){
-        loadMeetings((this.props.location.pathname).split('/')[2],this.onLoadSuccess);
+    componentDidMount() {
+        loadMeetings((this.props.location.pathname).split('/')[2], this.onLoadSuccess);
     }
 
-    onLoadSuccess(response){
+    onLoadSuccess(response) {
         this.setState({meetings: response});
         console.log(this.state.meetings)
     }
 
     render() {
-        let content =<h3 className="text-muted">You have not created any meetings yet.</h3>;
+        let content = <h3 className="text-muted">You have not created any meetings yet.</h3>;
 
-        if(this.state.meetings.length > 0){
-            content =  this.state.meetings.map((el,i) => {
+        if (this.state.meetings.length > 0) {
+            content = this.state.meetings.map((el, i) => {
                 return <Meeting key={i} topic={el.topic} time={el.time} date={el.date} meetingId={el._id}/>
-            })}
+            })
+        }
         return (
             <div>
                 <h1>My Meetings</h1>
