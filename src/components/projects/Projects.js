ec604becd527f4c4c127adc092ff737dacb7933b
AlisaTerzieva <alisa.terzieva@gmail.com>
2016-12-07 02:58:56 +0200

    More CSS (team forms)

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -5,27 +5,35 @@ import {Link} from 'react-router';
 export default class Team extends Component{
     render(){
         return(
-            <div className="col-sm-4" id={this.props.teamId}>
-                <div className="card card-block">
-                    <p className="spanner">Name</p>
-                    <h3 className="card-title">{this.props.name}</h3>
-                    <span className="spanner">Description</span>
+        <form id="myProjects">
+            <div id={this.props.teamId}>
+            <div id="teamName">
+                    <h4 className="description">Name</h4>
+                    <h2>{this.props.name}</h2>
+            </div>
+                    <h4 className="description">Description</h4>
                     <p>{this.props.description || "Description is empty"}</p>
-                    <span className="spanner">Beginning</span>
+                    <h4 className="description">Beginning</h4>
                     <p>{this.props.beginning}</p>
-                    <span className="spanner">Deadline</span>
+                    <h4 className="description">Deadline</h4>
                     <p>{this.props.deadline}</p>
-                    <span className="spanner">Team Management</span>
+                    <h4 className="description">Team Management</h4>
+                    <div id="teamManagementBtns">
                     <Link to={"/edit/" + this.props.teamId} className="btn btn-primary">Edit</Link>
                     <Link to={"/delete/" + this.props.teamId} className="btn btn-danger">Delete</Link>
-                    <span className="spanner">User Management</span>
+                    </div>
+                    <h4 className="description">User Management</h4>
+                <div id="userManagementBtns">
                     <Link to={this.props.teamId + "/users"} className="btn btn-primary">Add Members</Link>
                     <Link to={this.props.teamId + "/users/remove"} className="btn btn-primary">Remove Members</Link>
-                    <span className="spanner">Meetings</span>
+                </div>
+                    <h4 className="description">Meetings</h4>
+                <div id="meetingsManagementBtns">
                     <Link to={'/meeting/'+ this.props.teamId + '/create' } className="btn btn-success">Create Meeting</Link>
                     <Link to={'/meeting/'+ this.props.teamId + '/edit' } className="btn btn-success">Edit Meetings</Link>
                 </div>
-            </div>
+                </div>
+        </form>
         )
     }
 }
